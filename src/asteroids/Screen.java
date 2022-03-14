e29a20dd03acdccad053189ef1ba86e911f4573d
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-14 16:22:56 +0000

    Creates an empty screen

--- src/asteroids/Screen.java
+++ src/asteroids/Screen.java
@@ -1,7 +1,30 @@
 package asteroids;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Screen {
+
+    // Reference: https://www.youtube.com/watch?v=5o3fMLPY7qY
+    public Screen() {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Screen");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing");
+        Screen testScreen = new Screen();
     }
 }
