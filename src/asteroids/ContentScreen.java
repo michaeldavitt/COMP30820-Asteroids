a1c187c3adbfcb224b446372eed564131731da8d
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-16 18:23:50 +0000

    Gets rid of TextLabel and Title classes and changes GameScreen class to ContentScreen

--- src/asteroids/ContentScreen.java
+++ src/asteroids/ContentScreen.java
@@ -5,8 +5,8 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
-public class GameScreen extends JPanel {
-    public GameScreen() {
+public class ContentScreen extends JPanel {
+    public ContentScreen() {
         this.setLayout(new BorderLayout());
         this.setBackground(Color.BLACK);
     }
--- src/asteroids/Main.java
+++ src/asteroids/Main.java
@@ -1,14 +1,28 @@
 package asteroids;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Main {
     public static void main(String[] args) {
-        Screen screen = new Screen(); // Creates basic screen
-        GameScreen gameScreen = new GameScreen(); // Creates screen where we will display our game
-        Title gameTitle = new Title("Welcome to Asteroids"); // Creates title panel describing our game
-        screen.add(gameTitle, BorderLayout.NORTH); // Adds the title to the screen
-        screen.add(gameScreen, BorderLayout.CENTER); // Adds the game screen to the basic screen
+        // First step, display a welcome screen to the user
+        Screen screen = new Screen(); // Creates generic screen to hold our game
+
+        // Creates specific screen where we will display a welcome message to the user
+        ContentScreen welcomeScreen = new ContentScreen();
+
+        JLabel gameTitle = new JLabel("Welcome to Asteroids", SwingConstants.CENTER); // Creates title label
+        JLabel pressEnter = new JLabel("Press Enter to Start", SwingConstants.CENTER); // Creates instruction
+        gameTitle.setForeground(Color.WHITE);
+        pressEnter.setForeground(Color.WHITE);
+
+        welcomeScreen.add(gameTitle, BorderLayout.NORTH); // Adds the title to the screen
+        welcomeScreen.add(pressEnter, BorderLayout.CENTER); // Adds press enter instruction to screen
+        screen.add(welcomeScreen, BorderLayout.CENTER); // Adds the game screen to the basic screen
+
         screen.setVisible(true); // Makes the screen visible
     }
 }
