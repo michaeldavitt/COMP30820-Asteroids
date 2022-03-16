0f92f71fc651d265ba8829253c17b53746bf2f8f
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-16 23:46:59 +0000

    Reimplements screen using JavaFX

--- src/asteroids/Main.java
+++ src/asteroids/Main.java
@@ -1,28 +1,56 @@
 package asteroids;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+//Java Program to create a Pane
+//and add labels and buttons to the pane
+//and relocate them to specific positions
+//and add it to the stage
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.*;
+//import javafx.scene.web.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 
-public class Main {
-    public static void main(String[] args) {
-        // First step, display a welcome screen to the user
-        Screen screen = new Screen(); // Creates generic screen to hold our game
+// Reference: https://www.youtube.com/watch?v=9XJicRt_FaI
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-        // Creates specific screen where we will display a welcome message to the user
-        ContentScreen welcomeScreen = new ContentScreen();
-
-        JLabel gameTitle = new JLabel("Welcome to Asteroids", SwingConstants.CENTER); // Creates title label
-        JLabel pressEnter = new JLabel("Press Enter to Start", SwingConstants.CENTER); // Creates instruction
-        gameTitle.setForeground(Color.WHITE);
-        pressEnter.setForeground(Color.WHITE);
-
-        welcomeScreen.add(gameTitle, BorderLayout.NORTH); // Adds the title to the screen
-        welcomeScreen.add(pressEnter, BorderLayout.CENTER); // Adds press enter instruction to screen
-        screen.add(welcomeScreen, BorderLayout.CENTER); // Adds the game screen to the basic screen
-
-        screen.setVisible(true); // Makes the screen visible
-    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+//		Stage stage = new Stage();
+		Group root = new Group();
+		Scene scene = new Scene(root, Color.BLACK);
+		
+		Text text = new Text();
+		text.setText("Welcome to Asteroids");
+		text.setX(100);
+		text.setY(50);
+		text.setFont(Font.font("Verdana", 50));
+		text.setFill(Color.WHITE);
+		root.getChildren().add(text);
+		
+		Image icon = new Image("asteroid.jpg");
+		stage.getIcons().add(icon);
+		stage.setTitle("Asteroids");
+		
+		stage.setFullScreen(true);
+		
+		stage.setScene(scene);
+		stage.show();
+		
+	}
 }
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module myfx {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens asteroids;
+}
