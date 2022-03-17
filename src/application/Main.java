6e2e6c8ef8f010483ac5c502440bd1499d25d183
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-17 01:13:22 +0000

    Adds scene builder and css styling to welcome screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,16 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+//import javafx.scene.image.Image;
+//import javafx.scene.layout.Pane;
+//import javafx.scene.paint.Color;
+//import javafx.scene.shape.Polygon;
+//import javafx.scene.text.Font;
+//import javafx.scene.text.Text;
 
 
 //Reference: https://www.youtube.com/watch?v=9XJicRt_FaI
@@ -20,29 +21,11 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
-//		Stage stage = new Stage();
-		Pane root = new Pane();
-		Scene scene = new Scene(root, Color.BLACK);
-		
-		Text text = new Text();
-		text.setText("Welcome to Asteroids");
-		text.relocate(700, 500);
-		text.setFont(Font.font("Verdana", 50));
-		text.setFill(Color.WHITE);
-		
-		
-		Image icon = new Image("asteroid.jpg");
-		stage.getIcons().add(icon);
-		stage.setTitle("Asteroids");
-		
-		Polygon ship = new Polygon();
-		ship.getPoints().setAll(200.0, 200.0, 300.0, 300.0, 200.0, 300.0);
-		ship.setFill(Color.WHITE);
-		stage.setFullScreen(true);
-		
-		root.getChildren().add(text);
-		root.getChildren().add(ship);
+		Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+		Scene scene = new Scene(root);
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		stage.setScene(scene);
 		stage.show();
 		
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module HelloFX {
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
