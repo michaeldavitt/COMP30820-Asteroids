7c00552662d81732659cfe1da17b8b85cdc90c10
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-18 00:26:45 +0000

    Adds three new screens - game over screen, enter name screen, and view high scores screen

--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -14,26 +14,78 @@ public class SceneController {
 	private Scene scene;
 	private Parent root;
 	
-	public void switchToGameScreen(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("Game_Screen.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		
-		String css = this.getClass().getResource("application.css").toExternalForm();
-		scene.getStylesheets().add(css);
-		
-		stage.setScene(scene);
-		stage.show();
-	}
-	
 	public void switchToWelcomeScreen(ActionEvent event) throws IOException {
+		// Generate the welcome screen
 		root = FXMLLoader.load(getClass().getResource("Welcome_Screen.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		
+		// Add styling to the welcome screen
 		String css = this.getClass().getResource("application.css").toExternalForm();
 		scene.getStylesheets().add(css);
 		
+		// Add the welcome screen to the window and show the window
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToGameScreen(ActionEvent event) throws IOException {
+		// Generate the game screen
+		root = FXMLLoader.load(getClass().getResource("Game_Screen.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		
+		// Add styling to the game screen
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
+		// Add the game screen to the window and show the window
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
+	public void switchToGameOverScreen(ActionEvent event) throws IOException {
+		// Generate the welcome screen
+		root = FXMLLoader.load(getClass().getResource("Game_Over_Screen.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		
+		// Add styling to the welcome screen
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
+		// Add the welcome screen to the window and show the window
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToEnterNameScreen(ActionEvent event) throws IOException {
+		// Generate the welcome screen
+		root = FXMLLoader.load(getClass().getResource("Enter_Name_Screen.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		
+		// Add styling to the welcome screen
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
+		// Add the welcome screen to the window and show the window
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToViewHighScoreScreen(ActionEvent event) throws IOException {
+		// Generate the welcome screen
+		root = FXMLLoader.load(getClass().getResource("View_High_Score_Screen.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		
+		// Add styling to the welcome screen
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
+		// Add the welcome screen to the window and show the window
 		stage.setScene(scene);
 		stage.show();
 	}
