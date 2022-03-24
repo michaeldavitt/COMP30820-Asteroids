6a046193881c89e738fddc4cc57fc68e7d731254
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2022-03-24 15:50:28 +0000

    Reimplements game screen using Pane class instead of fxml/scene builder

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+//import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 //import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
+//import javafx.scene.input.KeyCode;
+//import javafx.scene.layout.AnchorPane;
 //import javafx.scene.paint.Color;
 //import javafx.scene.shape.Polygon;
 //import javafx.scene.image.Image;
@@ -15,6 +21,7 @@ import javafx.scene.layout.AnchorPane;
 //import javafx.scene.shape.Polygon;
 //import javafx.scene.text.Font;
 //import javafx.scene.text.Text;
+import javafx.scene.layout.Pane;
 
 
 //Reference: https://www.youtube.com/watch?v=9XJicRt_FaI
@@ -29,7 +36,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		// Create the welcome screen
-		AnchorPane root = FXMLLoader.<AnchorPane>load(getClass().getResource("Welcome_Screen.fxml"));
+		Pane root = new Pane();
 		Scene scene = new Scene(root);
 		root.setPrefSize(screenX, screenY);
 		
@@ -40,6 +47,44 @@ public class Main extends Application {
 		// Add image to screen
 		Image icon = new Image("asteroid.jpg");
 		stage.getIcons().add(icon);
+		
+		// Adds buttons to the screen to switch scenes
+		SceneController sceneController = new SceneController();
+		Button launchGameButton = new Button("Begin Game");
+		launchGameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					sceneController.switchToLevelScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		launchGameButton.setTranslateX(screenX / 2);
+		launchGameButton.setTranslateY(screenY / 2);
+		root.getChildren().add(launchGameButton);
+		
+		Button viewControlsButton = new Button("Controls");
+		viewControlsButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					sceneController.switchToControlsScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		viewControlsButton.setTranslateX(screenX / 2);
+		viewControlsButton.setTranslateY(screenY / 4);
+		root.getChildren().add(viewControlsButton);
 		
 		// Put welcome screen onto the main screen
 		stage.setScene(scene);
--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -3,14 +3,18 @@ package application;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 //import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 //import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.AnchorPane;
+//import javafx.scene.input.KeyEvent;
+//import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -21,19 +25,19 @@ import javafx.scene.Node;
 public class SceneController {
 	private Stage stage;
 	private Scene scene;
-	private AnchorPane root;
+	private Pane root;
 	private final int screenX = 800;
 	private final int screenY = 600;
 	private int currentLevel = 1;
 	
 	public void switchToWelcomeScreen(ActionEvent event) throws IOException {
-		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Welcome_Screen.fxml"));
+		// Create the welcome screen
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
 		root.setPrefSize(screenX, screenY);
-		scene = new Scene(root);
 		
-		// Add styling to the welcome screen
+		// Style the welcome screen
 		String css = this.getClass().getResource("application.css").toExternalForm();
 		scene.getStylesheets().add(css);
 		
@@ -41,7 +45,43 @@ public class SceneController {
 		Image icon = new Image("asteroid.jpg");
 		stage.getIcons().add(icon);
 		
-		// Add the welcome screen to the window and show the window
+		// Adds buttons to the screen to switch scenes
+		Button launchGameButton = new Button("Begin Game");
+		launchGameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToGameScreen(arg0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		launchGameButton.setTranslateX(screenX / 2);
+		launchGameButton.setTranslateY(screenY / 2);
+		root.getChildren().add(launchGameButton);
+		
+		Button viewControlsButton = new Button("Controls");
+		viewControlsButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToControlsScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		viewControlsButton.setTranslateX(screenX / 2);
+		viewControlsButton.setTranslateY(screenY / 4);
+		root.getChildren().add(viewControlsButton);
+		
+		// Put welcome screen onto the main screen
 		stage.setScene(scene);
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
@@ -49,13 +89,13 @@ public class SceneController {
 	}
 	
 	public void switchToControlsScreen(ActionEvent event) throws IOException {
-		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Controls_Screen.fxml"));
+		// Generate the controls screen
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
 		root.setPrefSize(screenX, screenY);
-		scene = new Scene(root);
 		
-		// Add styling to the welcome screen
+		// Add styling to the controls screen
 		String css = this.getClass().getResource("application.css").toExternalForm();
 		scene.getStylesheets().add(css);
 		
@@ -63,16 +103,54 @@ public class SceneController {
 		Image icon = new Image("asteroid.jpg");
 		stage.getIcons().add(icon);
 		
-		// Add the welcome screen to the window and show the window
+		// Add the controls screen to the window and show the window
 		stage.setScene(scene);
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
 		stage.show();
+		
+		// Adds buttons to the screen to switch scenes
+		Button launchGameButton = new Button("Begin Game");
+		launchGameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToGameScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		launchGameButton.setTranslateX(screenX / 2);
+		launchGameButton.setTranslateY(screenY / 2);
+		root.getChildren().add(launchGameButton);
+		
+		Button returnToWelcomeButton = new Button("Return to Main Menu");
+		returnToWelcomeButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToWelcomeScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		returnToWelcomeButton.setTranslateX(screenX / 2);
+		returnToWelcomeButton.setTranslateY(screenY / 4);
+		root.getChildren().add(returnToWelcomeButton);
+		
 	}
 	
 	public void switchToGameScreen(ActionEvent event) throws IOException {
 		// Generate the game screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Game_Screen.fxml"));
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		root.setPrefSize(screenX, screenY);
 		scene = new Scene(root);
@@ -100,6 +178,44 @@ public class SceneController {
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
 		
+		// Adds buttons to the screen to switch scenes
+		Button switchLevelButton = new Button("View Level Screen");
+		switchLevelButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToLevelScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		switchLevelButton.setTranslateX(screenX / 3);
+		switchLevelButton.setTranslateY(screenY / 3);
+		root.getChildren().add(switchLevelButton);
+		
+		Button switchToGameOverScreenButton = new Button("End game");
+		switchToGameOverScreenButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToGameOverScreen(arg0);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		switchToGameOverScreenButton.setTranslateX(screenX / 2);
+		switchToGameOverScreenButton.setTranslateY(screenY / 4);
+		root.getChildren().add(switchToGameOverScreenButton);
+				
+		
 		// Get ship to rotate
 		stage.getScene().setOnKeyPressed(e -> {
 			e.consume();
@@ -119,7 +235,7 @@ public class SceneController {
 	
 	public void switchToGameOverScreen(ActionEvent event) throws IOException {
 		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Game_Over_Screen.fxml"));
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		root.setPrefSize(screenX, screenY);
 		scene = new Scene(root);
@@ -137,11 +253,29 @@ public class SceneController {
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
 		stage.show();
+		
+		// Adds buttons to the screen to switch scenes
+		Button switchToEnterNameButton = new Button("Enter Name");
+		switchToEnterNameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToEnterNameScreen(arg0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		switchToEnterNameButton.setTranslateX(screenX / 2);
+		switchToEnterNameButton.setTranslateY(screenY / 2);
+		root.getChildren().add(switchToEnterNameButton);
 	}
 	
 	public void switchToEnterNameScreen(ActionEvent event) throws IOException {
 		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Enter_Name_Screen.fxml"));
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		root.setPrefSize(screenX, screenY);
 		scene = new Scene(root);
@@ -159,11 +293,29 @@ public class SceneController {
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
 		stage.show();
+		
+		// Adds buttons to the screen to switch scenes
+		Button switchToHighScoresButton = new Button("High Scores");
+		switchToHighScoresButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToViewHighScoreScreen(arg0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		switchToHighScoresButton.setTranslateX(screenX / 2);
+		switchToHighScoresButton.setTranslateY(screenY / 2);
+		root.getChildren().add(switchToHighScoresButton);
 	}
 	
 	public void switchToViewHighScoreScreen(ActionEvent event) throws IOException {
 		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("View_High_Score_Screen.fxml"));
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		root.setPrefSize(screenX, screenY);
 		scene = new Scene(root);
@@ -201,22 +353,28 @@ public class SceneController {
 		stage.setResizable(false);
 		stage.show();
 		
-		stage.getScene().setOnKeyPressed(e -> {
-			e.consume();
-			if (e.getCode() == KeyCode.ENTER) {
+		// Adds buttons to the screen to switch scenes
+		Button switchToWelcomeButton = new Button("Return to main menu");
+		switchToWelcomeButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
 				try {
-					switchToGameScreen(event);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+					switchToWelcomeScreen(arg0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
 			}
 		});
+		
+		switchToWelcomeButton.setTranslateX(50);
+		switchToWelcomeButton.setTranslateY(50);
+		root.getChildren().add(switchToWelcomeButton);
 	}
 	
 	public void switchToLevelScreen(ActionEvent event) throws IOException {
 		// Generate the welcome screen
-		root = FXMLLoader.<AnchorPane>load(getClass().getResource("Level_Screen.fxml"));
+		root = new Pane();
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		root.setPrefSize(screenX, screenY);
 		scene = new Scene(root);
@@ -227,7 +385,7 @@ public class SceneController {
 		
 		// Add text representing the user's current level
 		Text levelText = new Text();
-		levelText.setText("Level " + currentLevel + " complete!");
+		levelText.setText("Level " + currentLevel);
 		levelText.setX(screenX / 4);
 		levelText.setY(screenY / 4);
 		levelText.setFont(Font.font("Verdana", 50));
@@ -239,12 +397,21 @@ public class SceneController {
 		
 		// Add text prompting the user to hit enter to begin next level
 		Text nextLevelText = new Text();
-		nextLevelText.setText("Press Enter to begin Level " + currentLevel);
+		nextLevelText.setText("Press Enter to continue");
 		nextLevelText.setX(screenX / 4);
 		nextLevelText.setY(screenY / 2);
 		nextLevelText.setFont(Font.font("Verdana", 30));
 		nextLevelText.setFill(Color.WHITE);
+		
+		Text pressEnterText = new Text();
+		pressEnterText.setText("Level " + currentLevel);
+		pressEnterText.setX(screenX / 2);
+		pressEnterText.setY(screenY * (3/4));
+		pressEnterText.setFont(Font.font("Verdana", 30));
+		pressEnterText.setFill(Color.WHITE);
+		
 		root.getChildren().add(nextLevelText);
+		root.getChildren().add(pressEnterText);
 		
 		// Add image to screen
 		Image icon = new Image("asteroid.jpg");
@@ -255,5 +422,36 @@ public class SceneController {
 		stage.setTitle("Asteroids");
 		stage.setResizable(false);
 		stage.show();
+		
+		// Adds buttons to the screen to switch scenes
+		Button switchToGameButton = new Button("Begin Game");
+		switchToGameButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					switchToGameScreen(arg0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		});
+		
+		switchToGameButton.setTranslateX(50);
+		switchToGameButton.setTranslateY(50);
+		root.getChildren().add(switchToGameButton);
+		
+		// Change scene when the user presses Enter
+//		stage.getScene().setOnKeyPressed(e -> {
+//			e.consume();
+//			if (e.getCode() == KeyCode.ENTER) {
+//				try {
+//					switchToGameScreen(event);
+//				} catch (IOException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//			}
+//		});
 	}
 }
